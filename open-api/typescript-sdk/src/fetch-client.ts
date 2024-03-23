8550ab9db27e03bc9d19c72b1bdc496fbf2346a4
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-23 19:53:06 -0400

    disable concurrency

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -871,7 +871,6 @@ export type JobSettingsDto = {
 };
 export type SystemConfigJobDto = {
     backgroundTask: JobSettingsDto;
-    duplicateDetection: JobSettingsDto;
     faceDetection: JobSettingsDto;
     library: JobSettingsDto;
     metadataExtraction: JobSettingsDto;
