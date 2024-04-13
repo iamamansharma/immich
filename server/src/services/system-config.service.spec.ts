0da6c4a33f287336038d1d745da778a4685afe23
mertalev <101130780+mertalev@users.noreply.github.com>
2024-04-13 14:34:40 -0400

    disable by default

--- server/src/services/system-config.service.spec.ts
+++ server/src/services/system-config.service.spec.ts
@@ -127,7 +127,7 @@ const updatedConfig = Object.freeze<SystemConfig>({
     previewSize: 1440,
     quality: 80,
     colorspace: Colorspace.P3,
-    extractEmbedded: true,
+    extractEmbedded: false,
   },
   newVersionCheck: {
     enabled: true,
