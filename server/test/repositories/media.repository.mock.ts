2b59c5c75a33ebc43ae6bacb2bc6ed02a083ebd1
mertalev <101130780+mertalev@users.noreply.github.com>
2024-04-13 03:18:55 -0400

    add tests

--- server/test/repositories/media.repository.mock.ts
+++ server/test/repositories/media.repository.mock.ts
@@ -3,6 +3,7 @@ import { IMediaRepository } from 'src/interfaces/media.interface';
 export const newMediaRepositoryMock = (): jest.Mocked<IMediaRepository> => {
   return {
     generateThumbhash: jest.fn(),
+    extract: jest.fn().mockResolvedValue(null),
     resize: jest.fn(),
     crop: jest.fn(),
     probe: jest.fn(),
